cab24b2a85d5f8e717c6ce702000df40290324ae
badruprasanth8 <badru8@hotmail.com>
2023-11-21 21:57:32 +0530

    Update main.py

--- pythonscript/main.py
+++ pythonscript/main.py
@@ -1,7 +1,18 @@
 # example.py
+import sys
 
-def add(a, b):
-    return a + b
+# Accepting command-line arguments
+arguments = sys.argv[1:]  # Exclude the first argument (script name)
+if len(arguments) != 2:
+    print("Please provide two numbers as arguments.")
+    sys.exit(1)
 
-result = add(5, 10)
-print("The result of addition is:", result)
+# Extracting arguments and performing addition
+try:
+    a = int(arguments[0])
+    b = int(arguments[1])
+    result = a + b
+    print("The result of addition is:", result)
+except ValueError:
+    print("Please provide valid numbers as arguments.")
+    sys.exit(1)
